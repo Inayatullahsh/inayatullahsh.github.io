3e269ccf2edcda5a80714b43f468738c70404eb5
Inayatullahsh <Inayatullahoffical@gmail.com>
2022-12-22 17:39:14 +0430

    Change navbar behaviour on scroll to fixed after 400px.

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+const quotes = [
+  `The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela`,
+  `The way to get started is to quit talking and begin doing. -Walt Disney`,
+  `Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. -Steve Jobs`,
+  `If life were predictable it would cease to be life, and be without flavor. -Eleanor Roosevelt`,
+  `If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey`,
+  `If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. -James Cameron`,
+  `Life is what happens when you're busy making other plans. -John Lennon`,
+]
+
+
+function newQuote() {
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  //console.log(randomNumber);
+  //document.getElementById('quoteWrapper').classList.remove('hidden');
+  document.getElementById('quoteWrapper').style.visibility = "visible";
+  document.getElementById('quote').innerHTML = quotes[randomNumber];
+}
+
+
+document.onreadystatechange = () => {
+
+  //newQuote();
+  if (document.readyState === "complete") {
+    document.getElementById('loader').classList.add('hidden');
+    //console.log("ready");
+  }
+
+  window.onscroll = (e) => {
+    const navbar = document.getElementById("navbar");
+
+    if (this.scrollY > 400) {
+      navbar.classList.add("sticky");
+      //console.table(this.oldScroll, this.scrollY);
+    } else {
+      if (navbar.classList.contains("sticky")) {
+        navbar.classList.remove("sticky");
+      }
+    }
+    this.oldScroll = this.scrollY;
+  }
+  // TODO: make navbar show on scroll up and hide on scroll down.
+  /*window.onscroll = (e) => {
+    const navbar = document.getElementById("navbar");
+
+    if (this.oldScroll > this.scrollY && this.scrollY > 0) {
+      navbar.classList.add("sticky");
+      //console.table(this.oldScroll, this.scrollY);
+    } else {
+      if (navbar.classList.contains("sticky")) {
+        navbar.classList.remove("sticky");
+      }
+    }
+    this.oldScroll = this.scrollY;
+  }
+*/
+
+};
--- todo.md
+++ todo.md
@@ -4,9 +4,8 @@
 1. [x] A simple navigation to go between pages.
 2. [x] A Heading section with a call to action that introduces yourself
 3. [x] An About section that has a bit of information about yourself (not too detailed)
-4. [ ] A link to your resume and social medias (e.g facebook, Twitter, GitHub, LinkedIn etc.)
+4. [x] A link to your resume and social medias (e.g facebook, Twitter, GitHub, LinkedIn etc.)
 5. [ ] A skills section that showcases your technical proficienceis
-6. [ ] A Projects/work section that showcases 3-5 of your best projects
-  1. [ ]
-7. [ ] A contact section with a form that allows people to directly contact you.
+6. [x] A Projects/work section that showcases 3-5 of your best projects
+7. [x] A contact section with a email, mobile number, get a quote and social media links.
 8. [ ] Use Inter or other awesome font family.
